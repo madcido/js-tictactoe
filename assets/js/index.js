0b3411f62a7c7af4ac8ac3c196386c88b0fb1acf
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-06-07 17:47:08 +0300

    Add custom alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,29 @@ const p2 = new Player('O');
 const board = new Board();
 const game = new Game(board, p1, p2);
 
+if(document.getElementById) {
+  window.alert = (message) => {
+    customAlert = document.createElement('div');
+    customAlert.innerHTML = message;
+    customAlert.setAttribute('class', 'custom-alert');
+    closeBtn = customAlert.appendChild(document.createElement('a'));
+    closeBtn.appendChild(document.createTextNode("Play Again!"));
+    closeBtn.href = "#";
+    closeBtn.focus();
+    closeBtn.onclick = () => {
+      display.resetBoard();
+      document.getElementById('board').removeChild(document.getElementsByClassName('custom-alert')[0]);
+      return false;
+    };
+    document.getElementById('board').appendChild(customAlert);
+  }
+}
+
 function playerMove(index) {
     if(game.isValidMove(index)) {
         display.handleMove(index, game.currentPlayer.mark);
         if(game.isOver()) {
             alert(game.message);
-            display.resetBoard();
             display.updateScores(p1, p2);
         }
         game.changeCurrentPlayer();
