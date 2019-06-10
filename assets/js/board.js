c1a69682119ef63f6201d11f425892efed89bf44
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-06-10 23:49:14 +0300

    Refactor through code review, Move class methods into prototype, move game winConditions into constructor

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,9 +1,11 @@
 function Board() {
-    this.tiles = new Array(9).fill(null);
-    this.reset = () => {
-        this.tiles = new Array(9).fill(null);
-    }
-    this.isFull = () => {
-        return !this.tiles.some((tile) => tile == null );
-    }
+  this.tiles = new Array(9).fill(null);
+}
+
+Board.prototype.reset = function() {
+  this.tiles = new Array(9).fill(null);
+}
+
+Board.prototype.isFull = function() {
+  return !this.tiles.some((tile) => tile == null);
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,43 +1,42 @@
 function Game(board, p1, p2) {
-    this.board = board;
-    this.p1 = p1;
-    this.p2 = p2;
-    this.currentPlayer = p1;
-
-    this.isValidMove = (index) => {
-        if((this.board.tiles[index] === 'X' || this.board.tiles[index] === 'O')) {
-            return false;
-        }
-        this.board.tiles[index] = this.currentPlayer.mark;
-        return true;
-    }
-
-    this.isOver = () => {
-        return (this.hasWinner() || this.board.isFull());
-    }
-
-    this.hasWinner = () => {
-        return Game.prototype.winConditions.some((combin) => {
-            return combin.every((index) => {
-                return this.board.tiles[index] != null &&
-                this.board.tiles[index] == this.board.tiles[combin[0]]
-            });
-        });
-    }
-
-    this.over = () => {
-        let message = "Tie!";
-        if (this.hasWinner()) {
-            this.currentPlayer.score++;
-            message = "Player " + this.currentPlayer.mark + " wins!";
-        }
-        this.board.reset();
-        return message;
-    }
-
-    this.changeCurrentPlayer = () => {
-        this.currentPlayer = (this.currentPlayer === p1) ? p2 : p1;
-    }
+  this.board = board;
+  this.p1 = p1;
+  this.p2 = p2;
+  this.currentPlayer = p1;
+  this.winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 }
 
-Game.prototype.winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+Game.prototype.isValidMove = function(index) {
+  if((this.board.tiles[index] === 'X' || this.board.tiles[index] === 'O')) {
+    return false;
+  }
+  this.board.tiles[index] = this.currentPlayer.mark;
+  return true;
+}
+
+Game.prototype.isOver = function() {
+  return (this.hasWinner() || this.board.isFull());
+}
+
+Game.prototype.hasWinner = function() {
+  return this.winConditions.some((combin) => {
+    return combin.every((index) => {
+      return this.board.tiles[index] != null &&
+        this.board.tiles[index] == this.board.tiles[combin[0]]
+    });
+  });
+}
+
+Game.prototype.over = function() {
+  let message = "Tie!";
+  if (this.hasWinner()) {
+    this.currentPlayer.score++;
+    message = "Player " + this.currentPlayer.mark + " wins!";
+  }
+  this.board.reset();
+  return message;
+}
+
+Game.prototype.changeCurrentPlayer = function() {
+  this.currentPlayer = (this.currentPlayer === p1) ? p2 : p1;
+}
