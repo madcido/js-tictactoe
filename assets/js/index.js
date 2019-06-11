a444c714cb73e20856ea630c2f958419fac3c08b
madcido <fabowow@gmail.com>
2019-06-11 13:01:54 -0300

    Add vs. computer mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ const p1 = new Player('X');
 const p2 = new Player('O');
 const board = new Board();
 const game = new Game(board, p1, p2);
+let mode = "";
 
 if(document.getElementById) {
   window.alert = (message) => {
@@ -22,15 +23,53 @@ if(document.getElementById) {
 }
 
 function playerMove(index) {
-    if(game.isValidMove(index)) {
+    if (game.isValidMove(index)) {
+        game.move(index);
         display.handleMove(index, game.currentPlayer.mark);
-        if(game.isOver()) {
+        if (game.isOver()) {
             alert(game.over());
             display.updateScores(p1, p2);
+        } else {
+            game.changeCurrentPlayer();
+            display.activePlayer(game.currentPlayer.mark);
+            if (mode == "AI") {
+                aiMove();
+            }
         }
-        game.changeCurrentPlayer();
-        display.activePlayer(game.currentPlayer.mark);
     }
 }
 
+function changeMode() {
+    if (document.getElementById("AI").checked) {
+        mode = "AI";
+    } else {
+        mode = "PVP";
+    }
+}
+
+function aiMove() {
+    let aiMove = randomMove();
+    game.move(aiMove);
+    display.handleMove(aiMove, game.currentPlayer.mark);
+    if (game.isOver()) {
+        alert(game.over());
+        display.updateScores(p1, p2);
+    }
+    game.changeCurrentPlayer();
+    display.activePlayer(game.currentPlayer.mark);
+}
+
+function randomMove() {
+    let move;
+    do {
+        move = Math.floor(Math.random()*9);
+    } while (!game.isValidMove(move));
+    return move;
+}
+
+function smartMove() {
+    //someday...
+}
+
+changeMode();
 display.resetBoard();
